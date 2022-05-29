9b78bc76250b5d71979a9702463cda10cb223dde
pedroCurioni <pedrocurioni@gmail.com>
2022-05-29 15:30:41 -0300

    adicionado base do pedido e da categoria

---
+++ src/main/java/com/residencia/ecommerce/service/CategoriaService.java
@@ -0,0 +1,35 @@
+package com.residencia.ecommerce.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.residencia.ecommerce.entity.Categoria;
+import com.residencia.ecommerce.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	CategoriaRepository categoriaRepository;
+
+	public List<Categoria> findAllCategoria() {
+		return categoriaRepository.findAll();
+	}
+
+	public Categoria findCategoriaById(Integer id) {
+		return categoriaRepository.findById(id).isPresent() ? categoriaRepository.findById(id).get() : null;
+	}
+
+	public Categoria saveCategoria(Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+
+	public Categoria updateCategoria(Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+
+	public void deleteCategoriaById(Integer id) {
+		categoriaRepository.deleteById(id);
+	}
+}
