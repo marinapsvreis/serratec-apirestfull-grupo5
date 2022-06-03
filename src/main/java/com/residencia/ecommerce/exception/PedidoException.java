13482d4fc19c29ba10abdbe6ddb9eda5017e8c69
Marina Portugal <marinapsvreis@gmail.com>
2022-06-03 20:27:05 -0300

    correcao put pedido estava sem data pedido recuperada do banco

---
+++ src/main/java/com/residencia/ecommerce/exception/PedidoException.java
@@ -0,0 +1,11 @@
+package com.residencia.ecommerce.exception;
+
+public class PedidoException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public PedidoException (String message) {
+		super(message);
+	}
+
+}
