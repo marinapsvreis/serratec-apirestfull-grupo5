b15ed6e306615602e29df709db0e1725b49ad6a1
Marina Portugal <marinapsvreis@gmail.com>
2022-06-06 17:14:53 -0300

    ajuste conferir descricao no produto no salvamento por foto e docx dos jsons

--- src/main/java/com/residencia/ecommerce/service/ProdutoService.java
+++ src/main/java/com/residencia/ecommerce/service/ProdutoService.java
@@ -23,82 +23,83 @@ public class ProdutoService {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private Arquivo2Service arquivo2Service;
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
-	
+
 	@Value("${pasta.arquivos.imagem}")
-    private Path path;
-	
-	public List<ProdutoDTO> findAllProduto(){
+	private Path path;
+
+	public List<ProdutoDTO> findAllProduto() {
 		List<Produto> produtoListEntity = produtoRepository.findAll();
 		List<ProdutoDTO> produtoListDTO = new ArrayList<>();
-		
-		for(Produto produto : produtoListEntity) {
+
+		for (Produto produto : produtoListEntity) {
 			produtoListDTO.add(toDTO(produto));
 		}
-		
+
 		return produtoListDTO;
 	}
-	
+
 	public ProdutoDTO findByIdProduto(Integer idProduto) {
-		ProdutoDTO produtoDTO = produtoRepository.findById(idProduto).isPresent() ?
-				toDTO(produtoRepository.findById(idProduto).get()) 
+		ProdutoDTO produtoDTO = produtoRepository.findById(idProduto).isPresent()
+				? toDTO(produtoRepository.findById(idProduto).get())
 				: null;
-		if(produtoDTO == null) {
+		if (produtoDTO == null) {
 			throw new NoSuchElementException("Não existe produto com o id " + idProduto);
-		}else {
+		} else {
 			return produtoDTO;
 		}
 	}
-	
+
 	public Produto saveProdutoDTO(ProdutoDTO produtoDTO) throws Exception {
 		produtoDTO.setDataCadastroProduto(new Date());
-		
+
 		List<Produto> listaProdutos = produtoRepository.findByDescricaoProduto(produtoDTO.getDescricaoProduto());
-		
-		if(!listaProdutos.isEmpty()) {
+
+		if (!listaProdutos.isEmpty()) {
 			throw new DescricaoProdutoException("Essa descrição ja foi utilizada em outro produto");
 		}
-			return produtoRepository.save(toEntity(produtoDTO));		
+		return produtoRepository.save(toEntity(produtoDTO));
 	}
-	
+
 	public ProdutoDTO updateProdutoDTO(Integer idProduto, ProdutoDTO produtoDTO) throws Exception {
 		produtoDTO.setIdProduto(idProduto);
 		produtoDTO.setDataCadastroProduto(new Date());
 		findByIdProduto(idProduto);
 		List<Produto> listaProdutos = produtoRepository.findByDescricaoProduto(produtoDTO.getDescricaoProduto());
-		
+
 		List<Integer> listaIdProdutosCadastrados = new ArrayList<>();
-		
-		for(Produto produto : listaProdutos) {
-			if(produto.getIdProduto() != produtoDTO.getIdProduto()) {
+
+		for (Produto produto : listaProdutos) {
+			if (produto.getIdProduto() != produtoDTO.getIdProduto()) {
 				listaIdProdutosCadastrados.add(idProduto);
 			}
 		}
-		
-		if(!listaIdProdutosCadastrados.isEmpty()) {
-			throw new DescricaoProdutoException("Essa descrição ja foi utilizada em outro produto de id:: " + listaIdProdutosCadastrados.get(0));
-		}		
-		
-		
+
+		if (!listaIdProdutosCadastrados.isEmpty()) {
+			throw new DescricaoProdutoException(
+					"Essa descrição ja foi utilizada em outro produto de id:: " + listaIdProdutosCadastrados.get(0));
+		}
+
 		produtoDTO.setIdProduto(idProduto);
 		return toDTO(produtoRepository.save(toEntity(produtoDTO)));
 	}
-	
+
 	public void deleteByIdProduto(Integer idProduto) {
 		findByIdProduto(idProduto);
 		produtoRepository.deleteById(idProduto);
-	
+
 	}
 
 	public Produto toEntity(ProdutoDTO produtoDTO) throws Exception {
 		Produto produto = new Produto();
-		
-		produto.setCategoria(categoriaService.toEntity(categoriaService.findCategoriaByIdDTO(produtoDTO.getIdCategoria())));
+
+		produto.setCategoria(
+				categoriaService.toEntity(categoriaService.findCategoriaByIdDTO(produtoDTO.getIdCategoria())));
 		produto.setIdProduto(produtoDTO.getIdProduto());
 		produto.setDataCadastroProduto(produtoDTO.getDataCadastroProduto());
 		produto.setDescricaoProduto(produtoDTO.getDescricaoProduto());
@@ -106,13 +107,13 @@ public class ProdutoService {
 		produto.setNomeProduto(produtoDTO.getNomeProduto());
 		produto.setQtdEstoqueProduto(produtoDTO.getQtdEstoqueProduto());
 		produto.setValorUnitario(produtoDTO.getValorUnitario());
-		
+
 		return produto;
 	}
-	
+
 	public ProdutoDTO toDTO(Produto produto) {
 		ProdutoDTO produtoDTO = new ProdutoDTO();
-		
+
 		produtoDTO.setIdCategoria(produto.getCategoria().getIdCategoria());
 		produtoDTO.setIdProduto(produto.getIdProduto());
 		produtoDTO.setDataCadastroProduto(produto.getDataCadastroProduto());
@@ -121,41 +122,40 @@ public class ProdutoService {
 		produtoDTO.setNomeProduto(produto.getNomeProduto());
 		produtoDTO.setQtdEstoqueProduto(produto.getQtdEstoqueProduto());
 		produtoDTO.setValorUnitario(produto.getValorUnitario());
-		
+
 		return produtoDTO;
 	}
 
 	public Produto saveProdutoComFoto(String produtoString, MultipartFile file) throws Exception {
 		ProdutoDTO novoProduto = new ProdutoDTO();
 		novoProduto.setDataCadastroProduto(new Date());
-		
+
+		try {
+			ObjectMapper objMapper = new ObjectMapper();
+			novoProduto = objMapper.readValue(produtoString, ProdutoDTO.class);
+		} catch (IOException e) {
+			System.out.println("Ocorreu um erro na conversão");
+		}
+
 		List<Produto> listaProdutos = produtoRepository.findByDescricaoProduto(novoProduto.getDescricaoProduto());
-		
-		if(!(listaProdutos.isEmpty())) {
+
+		if (!(listaProdutos.isEmpty())) {
 			throw new DescricaoProdutoException("Essa descrição ja foi utilizada em outro produto");
 		}
-        
-        try {
-            ObjectMapper objMapper = new ObjectMapper();
-            novoProduto = objMapper.readValue(produtoString, ProdutoDTO.class);
-        }catch(IOException e) {
-            System.out.println("Ocorreu um erro na conversão");
-        }
-        
-        Produto produtoSalvo = produtoRepository.save(toEntity(novoProduto));
-        
-        String fileName = "produto." + produtoSalvo.getIdProduto() + ".image.png";
-        
-        arquivo2Service.criarArquivo(fileName, file);        
-        
-        try {
-            produtoSalvo.setNomeImagemProduto(path.resolve(fileName).toRealPath().toString());
-        }catch(IOException e) {
-            e.printStackTrace();
-        }
 
-        return produtoRepository.save(produtoSalvo);
+		Produto produtoSalvo = produtoRepository.save(toEntity(novoProduto));
+
+		String fileName = "produto." + produtoSalvo.getIdProduto() + ".image.png";
+
+		arquivo2Service.criarArquivo(fileName, file);
+
+		try {
+			produtoSalvo.setNomeImagemProduto(path.resolve(fileName).toRealPath().toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return produtoRepository.save(produtoSalvo);
 	}
 
-	
 }
